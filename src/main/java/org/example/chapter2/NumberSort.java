cab09ea2fe6c58b16f13d74bb6a7df0f2af4d298
jhmoon <jhmoon@howser.co.kr>
2024-06-30 02:47:17 +0900

    수 정렬하기

---
+++ src/main/java/org/example/chapter2/NumberSort.java
@@ -0,0 +1,28 @@
+package org.example.chapter2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class NumberSort {
+
+  public static void main(String[] args) throws IOException {
+    //백만단위 이상 버퍼드 리더
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(bufferedReader.readLine());
+    int [] cnt = new int[10001];
+    for(int i = 0; i< n; i++) {
+      cnt[Integer.parseInt(bufferedReader.readLine())]++;
+    }
+    for(int i = 1; i< 10001; i++) {
+      while (cnt[i] --> 0) {
+        bufferedWriter.write(i + "\n");
+      }
+    }
+    bufferedWriter.flush();
+  }
+}
