0286f2afa408b5cbf8cfd6421eb75aa7f6b6876a
jhmoon <jhmoon@howser.co.kr>
2024-07-04 01:31:52 +0900

    유레카이론

---
+++ src/main/java/org/example/chapter2and3/GuardTheCastle.java
@@ -0,0 +1,125 @@
+package org.example.chapter2and3;
+
+import java.util.Scanner;
+
+public class GuardTheCastle {
+  //성지키기
+//  public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    int n = scanner.nextInt();
+//    int m = scanner.nextInt();
+//    char[][] map = new char[n][m];
+//    for (int idx = 0; idx < n; idx++) {
+//      map[idx] = scanner.next().toCharArray();
+//    }
+//    //각 행/열 에 대해 경비원이 있는지 확인
+//    int existRowCount = 0;
+//    for (int idx = 0; idx < n; idx++) {
+//      boolean isExist = false;
+//      for (int i = 0; i< m; i++) {
+//        if (map[idx][i] == 'X') {
+//          isExist = true;
+//          break;
+//        }
+//      }
+//      if (isExist) existRowCount++;
+//    }
+//
+//    int existColCount = 0;
+//    for (int idx =0; idx < m; idx++) {
+//      boolean isExist = false;
+//      for(int i = 0; i < n; i++ ) {
+//        if (map[n][m] == 'X') {
+//          isExist = true;
+//          break;
+//        }
+//      }
+//      if (isExist) existColCount++;
+//    }
+//
+//    //보호받지 못한 행 열 개수 구함
+//    int needRowCount = 0;
+//    needRowCount = n - existRowCount;
+//    int needColCount = 0;
+//    needColCount = m - existColCount;
+//    //둘중 큰 값 출력
+//    System.out.println(Math.max(needRowCount, needColCount));
+//  }
+
+  //
+//  public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    int height = scanner.nextInt();
+//    int width = scanner.nextInt();
+//    int count = 0;
+//    String [][] inputArray = new String[height][width];//맞나?
+//    for (int idx = 0; idx < height; idx++) {
+//      for(int i = 0; i < inputArray[idx].length; i++) {
+//        inputArray[idx][i] = scanner.next();
+//      }
+//    }
+//
+//    boolean [][] outputArray = new boolean[height][width];
+//    for (int idx = 0; idx < height; idx++) {
+//      for(int i = 0; i < inputArray[idx].length; i++) {
+//        if(!inputArray[idx][i].equals("X")) outputArray[idx][i] = false;
+//      }
+//    }
+//    for (int idx = 0; idx < height; idx++) {
+//      for (int idx = 0; idx < height; idx++) {
+//        inputArray[idx][WidthCnt];
+//      }
+//
+//    }
+//
+//    for (int idx = 0; idx < height; idx++) {
+//      for(int i = 0; i < inputArray[idx].length; i++) {
+//        if(!inputArray[idx][i].equals("X")) outputArray[idx][i] = false;
+//      }
+//
+//    }
+//
+//
+////    for (int idx =0; idx < inputArray.length; idx++) {
+////      if(!inputArray[idx].contains("X")) count++;
+////    }
+//    System.out.println(count);
+//  }
+
+//  public static void main(String[] args) {
+//
+//  }
+  public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   int n = scanner.nextInt();
+   int m = scanner.nextInt();
+   char[][] map = new char[n][m];
+   for (int idx = 0; idx < n; idx++) {
+     map[idx] = scanner.next().toCharArray();
+   }
+   boolean [] existRowArr = new boolean[n];
+   boolean [] existColArr = new boolean[m];
+   for (int idx = 0; idx < n; idx++) {
+     for (int i = 0; i< m; i++) {
+        if (map[idx][i] == 'X') {
+          existRowArr[idx] = true;
+          existColArr[i] = true;
+        }
+     }
+   }
+
+   int needRowCount = n;
+   int needColCount = m;
+   for (int idx = 0; idx < n; idx ++) {
+     if (existRowArr[idx]) needRowCount --;
+   }
+   for (int idx = 0; idx < m; idx ++) {
+     if (existColArr[idx]) needColCount --;
+   }
+
+   //둘중 큰 값 출력
+   System.out.println(Math.max(needRowCount, needColCount));
+ }
+
+
+}
---
+++ src/main/java/org/example/chapter2and3/LineUp.java
@@ -0,0 +1,70 @@
+package org.example.chapter2and3;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LineUp {
+
+  //줄 세우기 오름차순
+  public static void main(String[] args) throws IOException {
+    System.setIn(new FileInputStream("src/main/java/org/example/file/input.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] s = br.readLine().split(" ");
+    int cnt = 0;
+    for (int i = 0; i < s.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (Integer.parseInt(s[j]) > Integer.parseInt(s[i])) {
+          int curVal = Integer.parseInt(s[i]);
+          for (int k = i; k > j; k--) {// 거꾸로
+            s[k] = s[k - 1];
+            cnt++;
+          }
+          s[j] = String.valueOf(curVal);
+        }
+      }
+
+    }
+    System.out.println(cnt);
+  }
+//  public static void main(String[] args) {
+////    Sca
+//    Scanner sc = new Scanner(System.in);
+//    int P = sc.nextInt();
+//
+//    while (P-- > 0) {
+//      int T = sc.nextInt();
+//      int[] h = new int[20];
+//
+//
+//      for (int i = 0; i < 20; i++)//20
+//        h[i] = sc.nextInt();
+//
+//      int cnt = 0;
+//      for (int i = 0; i < 20; i++) {
+//
+//        for (int j = 0; j < i; j++) {
+//
+//          if (h[j] > h[i]) {
+//            int myH = h[i];
+//
+//            for (int k = i; k > j; k--) {
+//              h[k] = h[k - 1];
+//              cnt++;
+//            }
+//
+//            h[j] = myH;
+//            break;
+//          }
+//
+//        }
+//
+//      }
+//
+//      System.out.println(T + " " + cnt);
+//    }
+//
+//
+//  }
+}
