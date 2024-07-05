38481b3a9aa94d0b4b3aabb71d1447d5ee92a0a5
jhmoon <jhmoon@howser.co.kr>
2024-07-06 01:02:52 +0900

    진번변환 2(1)

---
+++ src/main/java/org/example/chapter4/BaseConversionTwo.java
@@ -0,0 +1,16 @@
+package org.example.chapter4;
+
+public class BaseConversionTwo {
+
+  public static void main(String[] args) {
+
+  }
+
+  //10진수 주어 질때 B 진법으로 바꿔 출력하라
+  //N을 B진법으로 나타냈을때 가장 큰 자릿수의 지수 K를 찾는다.
+  //N에 들어갈 수 있는 b항의 자릭밧을 k를 구한다.
+  //다음  자릿수의 표헌 범위를 넘는 부분을 제외 하고 2로 돌아가 다음 자릿구 구한다.
+
+
+
+}
